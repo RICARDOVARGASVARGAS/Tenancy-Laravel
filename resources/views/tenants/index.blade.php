3b413866ccb1c7950ef014c4969d9d48b806c54b
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2023-08-04 08:55:00 -0500

    Creacion de estilos

--- resources/views/tenants/index.blade.php
+++ resources/views/tenants/index.blade.php
@@ -1,7 +1,15 @@
 <x-app-layout>
-   <x-slot name="header">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             Inquilinos
         </h2>
     </x-slot>
+
+    <x-container class='py-12'>
+        <div class="flex justify-end">
+            <a href="{{ route('tenants.create') }}" class="btn btn-blue">
+                Nuevo
+            </a>
+        </div>
+    </x-container>
 </x-app-layout>
