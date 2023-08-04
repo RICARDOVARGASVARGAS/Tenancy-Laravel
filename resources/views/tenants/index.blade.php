158324e8327cfd2323e08d7c9ad495f158706a99
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2023-08-04 09:51:20 -0500

    Listar Tenancy

--- resources/views/tenants/index.blade.php
+++ resources/views/tenants/index.blade.php
@@ -6,10 +6,50 @@
     </x-slot>
 
     <x-container class='py-12'>
-        <div class="flex justify-end">
+        <div class="flex justify-end pb-6">
             <a href="{{ route('tenants.create') }}" class="btn btn-blue">
                 Nuevo
             </a>
         </div>
+
+
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Dominio
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tenants as $tenant)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $tenant->id }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $tenant->domains->first()->domain }}
+                            </td>
+                            <td class="px-6 py-4">
+                                <div class="flex justify-end">
+                                    <a href="{{ route('tenants.edit', $tenant) }}" class="btn btn-green">
+                                        Editar
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
     </x-container>
 </x-app-layout>
